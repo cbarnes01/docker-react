39acb7bfd481c79fba11467e83b637c48dab61db
cbarnes <christopher.barnes01@gmail.com>
2021-12-16 21:47:43 -0500

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello Dawg!
+         Updated in feature
         </p>
         <a
           className="App-link"
